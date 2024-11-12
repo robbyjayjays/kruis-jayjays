262fa680ac28d40f99cf89aab69acc017e18ad5f
Robby Cloetens <robby@jayjays.be>
2024-11-12 13:14:58 +0100

    toast and router dom

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../assets/css/login.css';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const Login = () => {
     const [email, setEmail] = useState('');
