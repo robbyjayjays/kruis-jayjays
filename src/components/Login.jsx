36f076f1cb22652df391d66d619ab60371a441e6
Robby Cloetens <robby@jayjays.be>
2024-11-12 13:15:52 +0100

    usestate

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../assets/css/login.css';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
