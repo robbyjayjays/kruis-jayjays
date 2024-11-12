a9a2ba4fcd9ad7e20c4897c05ad47fbdf9665efc
Robby Cloetens <robby@jayjays.be>
2024-11-12 13:54:35 +0100

    navbar to homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Navbar from './Navbar';
 
 const Home = () => {
     // State to hold the username (part of the email before the @)
@@ -20,6 +21,7 @@ const Home = () => {
 
     return (
         <div>
+            <Navbar />
             <ToastContainer />
             <h1>Homepage!</h1> {/* Display only the part before the @ */}
         </div>
