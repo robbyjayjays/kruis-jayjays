a10132553536c4d1f65c303b5a2034fcb474c1c7
Robby Cloetens <robby@jayjays.be>
2024-11-12 13:59:47 +0100

    css the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
+import '../assets/css/navbar.css';
 
 const Navbar = () => {
     const navigate = useNavigate();
