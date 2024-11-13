e93e4d39eceedb6b87e64f47f74fd83d5b64d88c
Robby Cloetens <robby@jayjays.be>
2024-11-13 10:09:39 +0100

    add info to account

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
+    return (
+      <>
+        <h2> Add Info to your account!</h2>
+        <h2> Created Workshops </h2>
+        <h2> Subscribed Workshops </h2>
+      </>
+    )
 }
 
 export default Profile
