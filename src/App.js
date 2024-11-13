12246d12c12979716a723e208c77e4661ce8ce1b
Robby Cloetens <robby@jayjays.be>
2024-11-13 10:04:32 +0100

    profile page link

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login'; // Make sure the path is correct
 import Home from './components/Home';
+import Profile from './components/Profile';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={ <Home />} />
+        <Route path="/profile" element= { <Profile />} />
       </Routes>
     </Router>
   );
