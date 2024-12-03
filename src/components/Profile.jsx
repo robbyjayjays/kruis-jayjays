8df07b9a03953c5c820b04e2f87207a38455da18
Robby <cloetensrobby@gmail.com>
2024-12-03 15:28:38 +0100

    preference name

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -356,7 +356,7 @@ const Profile = () => {
                                 <option value="">Selecteer eetvoorkeur</option>
                                 {dietaryOptions.map((option) => (
                                     <option key={option.id} value={option.preference_name}>
-                                        {option.name}
+                                        {option.preference_name}
                                     </option>
                                 ))}
                             </select>
