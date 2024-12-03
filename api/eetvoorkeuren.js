aa8aac44462c9b185c3916379eb6274a65b7148a
Robby <cloetensrobby@gmail.com>
2024-12-03 15:24:03 +0100

    pool fix for api

--- api/eetvoorkeuren.js
+++ api/eetvoorkeuren.js
@@ -1,3 +1,21 @@
+import pkg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.PORT || 5432,
+  ssl: {
+    rejectUnauthorized: false, // Necessary for many cloud providers
+  },
+});
+
 export default async function handler(req, res) {
     if (req.method === 'GET') {
         try {
